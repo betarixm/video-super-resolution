db0431de415c5b9c341f812b7cbce4505dd2f4ac
beta-lux <beta.underalpha@gmail.com>
2021-06-03 13:36:33 +0900

    LR scheduling

--- train.py
+++ train.py
@@ -10,13 +10,22 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     verbose=1
 )
 
+lr_schedule = tf.keras.optimizers.schedules.CosineDecayRestarts(
+    initial_learning_rate=0.001,
+    first_decay_steps=100
+)
+
 
 def train_and_evaluate():
     strategy = tf.distribute.MirroredStrategy()
     (X_train, y_train), (X_valid, y_valid) = load_data()
     with strategy.scope():
         model = OurModel()
-        model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.Huber(delta=0.01))
+        model.compile(
+            optimizer=tf.keras.optimizers.Nadam(learning_rate=lr_schedule),
+            loss=tf.keras.losses.Huber(delta=0.01)
+        )
+
         history = model.fit(
             X_train,
             y_train,
